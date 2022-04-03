68ec640354793db319c7e5f801596630e5956b8c
j-guerreiro <joel.borghi@gmail.com>
2022-04-03 18:27:30 -0300

    update - construindo classe usuário, atribuindo getters, setters, criando novo obj e atribuind valores às suas propriedades

--- user.php
+++ user.php
@@ -2,16 +2,93 @@
 
   class User
   {
-    public $nome;
-    public $sobrenome;
-    public $contadorSeguidores;
+    public string $nome;
+    public string $sobreNome;
+    public string $email;
+    public int $contadorSeguidores;
+    public string $login;
+    public string $senha;
+
+    public function setNome(string $nome): void
+    {
+      $this-> $nome = $nome;
+    }
+
+    public function setSobrenome(string $sobreNome): void
+    {
+      $this->$sobreNome = $sobreNome;
+    }
+
+    public function setEmail(string $email): void
+    {
+      $this->$email = $email;
+    }
+
+    public function setcontadorSeguidores(int $contadorSeguidores): void
+    {
+      $this->$contadorSeguidores = $contadorSeguidores;
+    }
+
+    public function setLogin(string $login): void
+    {
+      $this->login = $login;
+    }
+
+    public function setSenha(string $senha): void
+    {
+      $this->senha = $senha;
+    }
+
+    public function getNome(): string
+    {
+      return $this->nome;
+    }
+
+    public function getSobrenome(): string
+    {
+      return $this->sobreNome;
+    }
+
+    public function getEmail(): string
+    {
+      return $this->email;
+    }
+
+    public function getcontadorSeguidores(): int
+    {
+      return $this->contadorSeguidores;
+    }
+
+    public function getLogin(): string
+    {
+      return $this->login;
+    }
+
+    public function getSenha(): string
+    {
+      return $this->senha;
+    }
 
   }
 
-  $joelUser = new $User();
+  $objUsuario = new User();
 
-  $joelUser->nome = 'Joel';
+  $objUsuario->nome = 'João';
+  $objUsuario->sobreNome = 'Silva';
+  $objUsuario->email = 'js@email.com';
+  $objUsuario->contadorSeguidores = 1000;
+  $objUsuario->login = 'jsilva2003';
+  $objUsuario->senha = 'cAd178#@_PqH';
 
-
-
+  echo "+--------------Dados do Usuário----------------+\n";
+  echo "| Nome: {$objUsuario->nome}                                   |\n";
+  echo "<---------------------------------------------->\n";
+  echo "| Sobrenome: {$objUsuario->sobreNome}                             |\n";
+  echo "<---------------------------------------------->\n";
+  echo "| Email: {$objUsuario->email}                          |\n";
+  echo "<---------------------------------------------->\n";
+  echo "| Seguidores: {$objUsuario->contadorSeguidores}                             |\n";
+  echo "<---------------------------------------------->\n";
+  echo "| Login: {$objUsuario->login}                            |\n";
+  echo "*----------------------------------------------*\n";
 ?>
